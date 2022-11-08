9943680a6e6778577771b3606ee49c6f598df0e0
q.thompson <q.thompson088@gmail.com>
2022-11-08 15:15:32 -0800

    'Hello User' script

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,20 @@
-for (i = 1, n = 0; n < 1024; i += 1) {
-    n = 2**i;
-    console.log(n);
-}
+// for (i = 1, n = 0; n < 1024; i += 1) {
+//     n = 2**i;
+//     console.log(n);
+// }
 
-n = 0
-exp = 1
-while (n < 1024) {
-    n = 2**exp;
-    console.log(n);
-    exp += 1;
-}
+// n = 0
+// exp = 1
+// while (n < 1024) {
+//     n = 2**exp;
+//     console.log(n);
+//     exp += 1;
+// }
+
+let input;
+do {
+    input = prompt('Please enter you name.');
+    console.log(Number(input));
+} while (!isNaN(Number(input)));
+
+alert(`Hello ${input}`)
