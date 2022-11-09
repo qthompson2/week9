cc7f32c76ed4bfa77593d7baa652a8c2abe66861
q.thompson <q.thompson088@gmail.com>
2022-11-08 16:12:50 -0800

    Obj loops

--- scripts/script.js
+++ scripts/script.js
@@ -19,30 +19,58 @@
 
 // alert(`Hello ${input}`)
 
-let fruits = ['Apple', 'Banana']
-fruits.push('Orange')
-fruits.pop()
-fruits.unshift('Mango')
-fruits.shift()
+// let fruits = ['Apple', 'Banana']
+// fruits.push('Orange')
+// fruits.pop()
+// fruits.unshift('Mango')
+// fruits.shift()
 
-console.log(fruits)
+// console.log(fruits)
 
-const person = {
-    name:['Bob', 'Smith'],
-    age:40,
-    gender:'male',
-    interests:['Music', 'Skiing']
-}
-
-// let x = '';
-
-// for (let item in person.name) {
-//     x += person.name[item] + ' '
+// const person = {
+//     name:['Bob', 'Smith'],
+//     age:40,
+//     gender:'male',
+//     interests:['Music', 'Skiing']
 // }
 
-// console.log(`My name is ${person.name[0]} ${person.name[1]}.  I am interested in ${person.interests[0]} and ${person.interests[1]}.`)
+// // let x = '';
 
-for (let key in person) {
-    console.log(key);
-    console.log(person[key]);
+// // for (let item in person.name) {
+// //     x += person.name[item] + ' '
+// // }
+
+// // console.log(`My name is ${person.name[0]} ${person.name[1]}.  I am interested in ${person.interests[0]} and ${person.interests[1]}.`)
+
+// for (let key in person) {
+//     console.log(key);
+//     console.log(person[key]);
+// }
+
+let student1 = {
+    ID: "A00022",
+    GPA: 3.0,
+    program: "CIT"
+};
+
+let student2 = {
+    ID: "A01000",
+    GPA: 3.1,
+    program: "CST"
+};
+
+let student3 = {
+    ID: "A00114",
+    GPA: 2.2,
+    program: "CIT"
+};
+let students = [student1, student2, student3];
+
+let heighestGPA = 0;
+
+for (let student of students) {
+    if (student.program === 'CIT' && student.GPA > heighestGPA) {
+        heighestGPA = student.GPA
+    }
 }
+console.log(heighestGPA)
